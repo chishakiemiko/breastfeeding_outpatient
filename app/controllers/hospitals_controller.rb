86eebf3f536e7402d9a61b1cbc32a250bbff6948
EC2 Default User <ec2-user@ip-172-31-12-162.ap-northeast-1.compute.internal>
2023-06-12 21:43:46 +0900

    患者作成画面のデザインのみ作成

---
+++ app/controllers/hospitals_controller.rb
@@ -0,0 +1,7 @@
+class HospitalsController < ApplicationController
+  def sign_in
+  end
+  
+  def sign_up
+  end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,22 @@
+class PatientsController < ApplicationController
+    def index
+        
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def show
+        
+    end
+    
+    def edit
+        
+    end
+end
+
