d7b10de295689eca6fc15e42508e28a3b3155eee
EC2 Default User <ec2-user@ip-172-31-9-14.ap-northeast-1.compute.internal>
2023-07-01 20:42:17 +0000

    editとフラッシュメッセージ日本語

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
       #新規登録時のストロングパラメータに「username, job, profile, avatar」の追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
-      devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
   end  
     
