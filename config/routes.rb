86b7f07380a401c6b3b61793c84f3cad2bcde565
EC2 Default User <ec2-user@ip-172-31-9-14.ap-northeast-1.compute.internal>
2023-07-03 09:52:35 +0000

    データベースとviewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :hospitals
   root 'patients#index'
   resources :patients
+  resources :breast_conditions
+  
     
     
   # get '/hospitals/sign_in', to: 'hospitals#sign_in'
