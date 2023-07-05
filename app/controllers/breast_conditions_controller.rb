01ebf721a309e614185b9a4b3fe2f441541fa371
EC2 Default User <ec2-user@ip-172-31-9-14.ap-northeast-1.compute.internal>
2023-07-05 11:46:26 +0000

    newとshow大枠

--- app/controllers/breast_conditions_controller.rb
+++ app/controllers/breast_conditions_controller.rb
@@ -1,5 +1,13 @@
 class BreastConditionsController < ApplicationController
+  def index
+    @Breast_conditions = Breast_condition.all
+  end
+    
   def new
-    @breast_condition = Breast_condition.new
+    
+  end
+  
+  def show
+    
   end
 end
